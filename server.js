072c80003c0e3f285aa44f2d8ed0fad370f89270
Boshra Jaber <boshrajaber94@gmail.com>
2021-02-28 19:54:45 +0200

    working task3

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 
 // load the server dependencies
 var express = require('express');
-const cors=require('cors');
-
+const cors = require('cors');
 let ejs = require('ejs');
-
 var app = express();
+const superagent = require('superagent');
+const { text } = require('express');
 app.use(cors());
 
-app.use(express.static('./public'));
 
 require('dotenv').config();
 
-const PORT=process.env.PORT;
+app.use(express.static('./public'));
+
+app.use(express.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT;
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -21,30 +25,65 @@ app.set('view engine', 'ejs');
 
 /********************************** **
 ***************END POINTS  ***********
-***************************************/ 
+***************************************/
 // index page 
-app.get('/hello', (req, res)=> {
+app.get('/hello', (req, res) => {
     console.log("the main route")
-	res.render('./pages/index');
+    res.render('./pages/index');
 });
 
 
-app.get('/searches/new', (req, res)=>{
+app.get('/searches/new', (req, res) => {
+    // console.log(res, req);
     res.render('./pages/searches/new')
 });
-app.get('/searches/show', (req, res)=>{
-   console.log(req.query);
-   
+
+
+
+app.post('/searches/show', (req, res) => {
+    console.log(req.body);
+    //{ searchQuery: 'hello', searchBy: 'title', search: 'search' }
+    //request google  google book api 
+
+    let baseAPIUrl = "https://www.googleapis.com/books/v1/volumes"
+    let searchQuery = req.body.searchQuery + "+" + req.body.searchBy;
+    let query = {
+        q: searchQuery,
+    }
+    superagent.get(baseAPIUrl).query(query).then(data => {
+        //console.log(data.body.items[0]);
+        var results = [];
+        for (let index = 0; index < 10; index++) {
+
+            const element = data.body.items[index];
+            const book = new Book(
+                element.volumeInfo.title,
+                element.volumeInfo.imageLinks.thumbnail,
+                element.volumeInfo.authors,
+                element.volumeInfo.description
+            );
+            results.push(book);
+
+        }
+        console.log(results);
+
+
+        // res.render('pages/searches.new')
+
+        //    res.sendFile('./thanks.html', { root: './public' });
+    });
 });
+
+
 /********************************** **
 ***************END POINTS  ***********
-***************************************/ 
-function Book(title,img,authorName,description) {
-    this.title=title || 'unknown title';
-    this.img=img || 'https://i.imgur.com/J5LVHEL.jpg';
-    this.authorName=authorName || 'unknown author';
-    this.description=description || 'unavailable description';
+***************************************/
+function Book(title, img, authorName, description) {
+    this.title = title || 'unknown title';
+    this.img = img || 'https://i.imgur.com/J5LVHEL.jpg';
+    this.authorName = authorName || 'unknown author';
+    this.description = description || 'unavailable description';
 }
-app.listen(PORT,()=>{
-    console.log('app is lestining in port ....',PORT);
+app.listen(PORT, () => {
+    console.log('app is lestining in port ....', PORT);
 });
