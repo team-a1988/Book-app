8bc3e9d5d681e51f201c55912af072192ce60ad0
motasemAlsqoor <motasem900khaled@gmail.com>
2021-03-01 15:18:13 +0200

    first feature

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 
 // load the server dependencies
-var express = require('express');
+const express = require('express');
 const cors = require('cors');
-let ejs = require('ejs');
-var app = express();
+const ejs = require('ejs');
+const pg = require('pg');
+// configuration
+require('dotenv').config();
+const app = express();
 const superagent = require('superagent');
 const { text } = require('express');
 app.use(cors());
+const client = new pg.Client(process.env.DATABASE_URL);
 
 
-require('dotenv').config();
+
 
 app.use(express.static('./public'));
-
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -38,8 +41,19 @@ app.get('/searches/new', (req, res) => {
     res.render('./pages/searches/new')
 });
 
-app.get('/', (req,res) =>{
-    res.render('./pages/index')
+app.get('/', (req, res) => {
+    console.log("hello i am here ...............................");
+    const query='SELECT author,title,isbn,image_url,description FROM books';
+    client.
+    query(query).
+    then(data=>{
+        console.log(data.rows);
+        res.render('./pages/index',{"books":data.rows})
+    }).catch(error=>{
+        console.log(error);
+        res.render('./pages/error', { "error": error })
+    });
+    
 })
 
 
@@ -54,7 +68,7 @@ app.post('/searches/show', (req, res) => {
         q: searchQuery,
     }
     superagent.get(baseAPIUrl).query(query).then(data => {
-        //console.log(data.body.items[0]);
+        // console.log(data.body.items);
         var results = [];
         for (let index = 0; index < 10; index++) {
             const element = data.body.items[index];
@@ -62,12 +76,16 @@ app.post('/searches/show', (req, res) => {
                 element.volumeInfo.title,
                 element.volumeInfo.imageLinks.thumbnail,
                 element.volumeInfo.authors,
-                element.volumeInfo.description
+                element.volumeInfo.description,
+                element.volumeInfo.industryIdentifiers
             );
             results.push(book);
+            console.log(results);
+
         }
         res.render('./pages/searches/show', { "results": results });
     }).catch(error => {
+        console.log(error);
         res.render('./pages/error', { "error": error })
     });
 });
@@ -77,13 +95,18 @@ app.post('/searches/show', (req, res) => {
 /********************************** **
 ***************END POINTS  ***********
 ***************************************/
-function Book(title, img, authorName, description) {
+function Book(title, img, authorName, description, isbn) {
     this.title = title || 'unknown title';
     this.img = img || 'https://i.imgur.com/J5LVHEL.jpg';
     this.img = secure(img);
-    this.authorName = authorName || 'unknown author';
+    this.authorName = formatAuthor(authorName) || 'unknown author';
     this.description = description || 'unavailable description';
+    this.isbn = formatIsbn(isbn) || "unavailable isbn";
 }
+/***************************************** 
+*****************************************
+***************helper********************
+*******************************************/
 function secure(url) {
     if (url[5] != 's') {
         var i = url.split("")
@@ -91,6 +114,22 @@ function secure(url) {
     }
     return i.join("");
 }
-app.listen(PORT, () => {
-    console.log('app is lestining in port ....', PORT);
+function formatIsbn(isbn) {
+    if (isbn.length!=0) 
+        return isbn[0].type + " " + isbn[0].identifier//isbn
+    return null;
+}
+function formatAuthor(author){
+    if(typeof author==typeof undefined) return null;
+    if (author.length!=0) 
+        return author.join(", ");
+    return null;
+}
+client.connect().then(()=>{
+    app.listen(PORT, () => {
+        console.log('app is lestining in port ....', PORT);
+    });
+}).catch(error=>{
+    console.log('error app is not lestining in port ....', error);
 });
+
